af5796397795116feffc399a63208a631b0d9dee
Nimit Bhargava <nimitbhargava@gmail.com>
2018-03-23 10:36:55 +0530

    All together

--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ import reducer from './reducers';
 
 const store  = createStore(reducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
